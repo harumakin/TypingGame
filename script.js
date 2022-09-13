23681bd4a2262305e009c5516049ca32d16d7945
harumakin <shirasaka0116@gmail.com>
2022-09-13 22:37:15 +0900

    Asynchronously retrieve a random sentence

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-const RANDOM_SENTENCE_URL_API = "https"
+// APIを叩く
+const RANDOM_SENTENCE_URL_API = "https://api.quotable.io/random";
+
+/* 非同期でランダムな文章を取得する */
+function GetRandomSentence() {
+    return fetch(RANDOM_SENTENCE_URL_API)
+        .then((response) => response.json())
+        .then((data) => console.log(data.content));
+}
+
+GetRandomSentence();
