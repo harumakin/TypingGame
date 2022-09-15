95909541b28720d5d4ac9d8afa62728137a6ee9b
harumakin <shirasaka0116@gmail.com>
2022-09-16 00:14:56 +0900

    謎のバグを出した

--- script.js
+++ script.js
@@ -1,11 +1,107 @@
 // APIを叩く
 const RANDOM_SENTENCE_URL_API = "https://api.quotable.io/random";
+// index.htmlから"typeDisplay"を持ってくる（divタグ）
+const typeDisplay = document.getElementById("typeDisplay");
+// index.htmlから"typeInput"を持ってくる（divタグ）
+const typeInput = document.getElementById("typeInput");
+
+const typeSound = new Audio("./audio/typing-sound.mp3");
+const wrongSound = new Audio("./audio/wrong.mp3");
+const correctSound = new Audio("./audio/correct.mp3")
+
+/* inputテキスト入力。合っているかどうかの判定。 */
+typeInput.addEventListener("input", () => {
+    // タイプ音をつける
+    typeSound.play();
+    typeSound.currentTime = 0;
+    
+    const sentenceArray = typeDisplay.querySelectorAll("span");
+    // console.log(sentenceArray);
+    //splitを使ってtypeInputの文字を1文字ずつ区切る
+    const arrayValue = typeInput.value.split("");
+    // console.log(arrayValue);
+    let coreect = true;
+    sentenceArray.forEach((characterSpan, index) => {
+        if ((arrayValue[index] == null)) {
+            characterSpan.classList.remove("correct");
+            characterSpan.classList.remove("incorrect");
+            correct = false;
+        } else if (characterSpan.innerText == arrayValue[index]) {
+            characterSpan.classList.add("correct");
+            characterSpan.classList.remove("incorrect");
+        } else {
+            characterSpan.classList.add("incorrect");
+            characterSpan.classList.remove("correct");
+
+            wrongSound.volume = 0.3;
+            wrongSound.play();
+            wrongSound.currentTime = 0;
+
+            correct = false;
+        }     
+    });
+
+    if(coreect == true) {
+        correctSound.play();
+        correctSound.currentTime = 0;
+        RenderNextSentence();
+    }
+});
+
+
+
 
 /* 非同期でランダムな文章を取得する */
 function GetRandomSentence() {
     return fetch(RANDOM_SENTENCE_URL_API)
         .then((response) => response.json())
-        .then((data) => console.log(data.content));
+        .then((data) => data.content);
 }
 
-GetRandomSentence();
+
+/* ランダムな文章を取得して、お題として表示する */
+async function RenderNextSentence() {
+    const sentence =  await GetRandomSentence();
+    console.log(sentence);
+
+    typeDisplay.innerText = "";
+    /* 文章を1文字ずつ分解して、spanタグを生成する */
+    let onetext = sentence.split("");
+    onetext.forEach((character) => {
+        const characterSpan = document.createElement("span");
+        characterSpan.innerText = character;
+        // console.log(characterSpan);
+        typeDisplay.appendChild(characterSpan);
+        // characterSpan.classList.add("correct");
+    });
+
+    /* テキストボックスの中身を消す */
+    typeInput.value = "";
+
+    StartTimer();
+}
+
+let startTime;
+let originTime = 30;
+function StartTimer() {
+    timer.innerText = originTime;
+    startTime = new Date();
+    // console.log(startTime);
+    setInterval(() => {
+        timer.innerText = originTime - getTimerTime(); //1秒ずれて呼び出される
+        if (timer.innerText <= 0) TimeUp();
+ }, 1000);
+}
+
+function getTimerTime() {
+    return Math.floor(
+        (new Date() - startTime) / 1000
+    ); //現在の時刻 -　1秒前の時刻 = 1s
+}
+
+function TimeUp() {
+    console.log("next sentence");
+    RenderNextSentence();
+}
+
+RenderNextSentence();
